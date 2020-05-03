f77404c97e32d1d83399b005f12a69d1ae221b41
Jennifer Stoos <jstoos@bitlaw.com>
2020-05-03 16:30:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel(word)
-  if word.match[aeiou] !=nil
+  if word.match/[aeiou]/ !=nil
     true
-  elsif word.match[aeiou] =nil
+  elsif word.match/[aeiou]/ =nil
     false
   end
 end
