1b51e86c070c8aa41a2d2fbcad8dd5121a3ca16e
Jennifer Stoos <jstoos@bitlaw.com>
2020-05-03 16:43:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,11 @@
 def starts_with_a_vowel?(word)
   if word.match(/\b[aeiou]\S/i) !=nil
     true
-  elsif word.match(/[^aeiou]/) !=nil
+  elsif word.match(/\b[^aeiou]\S/i) !=nil
     false
   end
+end
+
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/(^[u][n].*[i][n][g]$)/i)
 end
